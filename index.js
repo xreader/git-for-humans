b58de801637cbf9041243d50d97715ae98f06485
xreader <xreader@gmail.com>
2017-11-09 22:50:58 +0100

    test ok 23

--- index.js
+++ index.js
@@ -4,18 +4,93 @@
  * Module dependencies.
  */
 
-var program = require('commander');
+// var program = require('commander');
+//
+// program
+//     .version('0.1.0')
+//     .command('change commit message', 'Edit last commit message')
+//     .option('-p, --peppers', 'Add peppers')
+//     .option('-P, --pineapple', 'Add pineapple')
+//     .option('-b, --bbq-sauce', 'Add bbq sauce')
+//     .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+//     .parse(process.argv);
+//
+// console.log('you ordered a pizza with:');
+// if (program.peppers) console.log('  - peppers');
+// if (program.pineapple) console.log('  - pineapple');
+// if (program.bbqSauce) console.log('  - bbq');
+// if (program['edit-last-commit-message']) console.log('  - edit last commit message');
+// console.log('  - %s cheese', program.cheese);
+//
 
-program
-    .version('0.1.0')
-    .option('-p, --peppers', 'Add peppers')
-    .option('-P, --pineapple', 'Add pineapple')
-    .option('-b, --bbq-sauce', 'Add bbq sauce')
-    .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
-    .parse(process.argv);
+const vorpal = require('vorpal')();
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+const spawn = require('child_process').spawn;
+const exec = require('child_process').exec;
+
+vorpal
+    .command('edit last commit message', 'Edit last commit message.')
+    .alias('elcm')
+    .action(function (args, callback) {
+        console.log('processing args=%s', args);
+        var cmd = 'git commit --amend';
+        console.log('executing %s', cmd);
+        execute('git', ['commit', '--amend'], function () {
+            callback();
+        })
+    });
+
+vorpal
+    .command('ls', 'list files in directory')
+    .action(function (args, callback) {
+        console.log('processing args=%s', args);
+        execute('ls', [], function () {
+            callback();
+        })
+    });
+
+vorpal
+    .command('stringify')
+    .option('-a, --amount <amt>', 'A number to stringify.')
+    .types({
+        string: ['a', 'amount']
+    })
+    .action(function (args, callback) {
+        this.log(args.options);
+        callback();
+    });
+
+vorpal
+    .delimiter('myapp$')
+    .show();
+
+function execute(command, arguments, callback) {
+    // child = spawn('ls', ['-lh', '/usr']);
+    // child = spawn(command, arguments);
+    child = exec(command + ' ' + arguments.join(' '));
+
+// use child.stdout.setEncoding('utf8'); if you want text chunks
+//     child.stdout.on('data', function (chunk) {
+//         // data from standard output is here as buffers
+//         console.log(chunk);
+//     });
+
+    child.stdout.pipe(process.stdout);
+
+    process.stdin.on('data', function (data) {
+        console.log('process.stdin!');
+        child.stdin.write(data);
+    });
+
+// since these are streams, you can pipe them elsewhere
+    child.stderr.on('data', function (chunk) {
+        // data from standard output is here as buffers
+        console.error(chunk);
+    });
+// child.stderr.pipe(dest);
+
+    child.on('close', function (code) {
+        console.log('child process exited with code code=%s', code);
+        callback();
+    });
+}
