692ff98c703b4b4e27942a8d855898026cf23e0f
xreader <xreader@gmail.com>
2017-12-07 16:18:10 +0100

    restore removed last commit with remove last commit commnad

---
+++ commands/remove-last-commit.js
@@ -0,0 +1,36 @@
+executer = require('./executor');
+
+module.exports = function(yargs){
+
+    yargs
+        .command(
+            'remove last commit',
+            'make a get HTTP request',
+            function (yargs) {
+                return yargs.option('u', {
+                    alias: 'url',
+                    describe: 'the URL to make an HTTP request to'
+                })
+            },
+            function (args) {
+                console.log('processing args=%s', args);
+
+                var commands = [
+                    {command: 'git', arguments: ['reset', 'HEAD^', '--hard']},
+                    {command: 'git', arguments: ['commit', '--amend']}
+                ]
+
+                var cmd = 'git reset HEAD^ --hard';
+                console.log('executing %s', cmd);
+                // response = executer.execute('git', ['commit', '--amend'], function () {
+                //     callback();
+                // })
+                response = executer.executeBatch(commands, function () {
+                    callback();
+                })
+            }
+        )
+        .help()
+        .argv
+
+}
