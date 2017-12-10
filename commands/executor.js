14141f0e910cf8939e02e5db9e4948bb0f7a0e26
xreader <xreader@gmail.com>
2017-12-10 18:00:10 +0100

    extract stdin input handling

--- commands/executor.js
+++ commands/executor.js
@@ -1,14 +1,15 @@
 const exec = require('child_process').exec;
 const spawn = require('child_process').spawn;
 
-function executeBatch (commands, callback) {
+function executeBatch (commands) {
     "use strict";
 
-    if (!commands) callback();
-
     var command = commands.shift();
+
+    if (!command) process.exit();
+
     execute(command.command, command.arguments, function () {
-        executeBatch(commands, callback);
+        executeBatch(commands);
     });
 }
 
@@ -18,19 +19,9 @@ function execute (command, arguments, callback) {
     child = exec(command + ' ' + arguments.join(' '));
 
     child.stdout.pipe(process.stdout);
-
-    stdin = process.stdin;
-    stdin.setEncoding('utf8');
-    stdin.setRawMode(true);
-    stdin.on('data', function (data) {
-        if (data == '\u0003') { process.exit(); }
-        child.stdin.write(data);
-    });
-    stdin.resume();
-
     child.on('close', function (code) {
-        console.log('child process exited with code code=%s', code);
-        callback();
+        // console.log('child process exited with code code=%s', code);
+        callback(code);
     });
 }
 
--- index.js
+++ index.js
@@ -7,3 +7,13 @@
 var yargs = require('yargs');
 
 require('./commands/remove-last-commit')(yargs);
+require('./commands/list')(yargs);
+
+stdin = process.stdin;
+stdin.setEncoding('utf8');
+stdin.setRawMode(true);
+stdin.on('data', function (data) {
+    if (data == '\u0003') { process.exit(); }
+    child.stdin.write(data);
+});
+stdin.resume();
