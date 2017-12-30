e81b2770de2094c824d5a0f7e1434ab78a05b71f
xreader <xreader@gmail.com>
2017-12-30 20:47:41 +0100

    using minimist as command interpreter

--- index.js
+++ index.js
@@ -1,21 +1,84 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
-var yargs = require('yargs');
-
+var argv = require('minimist')(process.argv.slice(2));
+argv.verbose && console.dir(argv);
 
 var command = require('./commands/command');
 var commands = require('./commands').commands;
 
-commands.forEach(function (cmd) {
-    command(yargs, cmd);
-});
+/**
+ * parse argumts
+ * @param argv
+ * @returns {command} list of commands that contains provided term in name
+ */
+parse = function (argv) {
+    argv.verbose && console.log('parse arguments...');
+    let matchedCommands = commands.filter(function (command) {
+        return command.command.indexOf(argv._.join(' ')) >= 0;
+    });
+    argv.verbose && console.log('command %s', JSON.stringify(matchedCommands));
+    return matchedCommands;
+}
 
+handle = function (matchedCommands, argv) {
 
-yargs.help().argv
+}
+
+/**
+ * prints help for command(s)
+ * @param matchedCommands
+ * @param argv
+ */
+help = function (matchedCommands, argv) {
+    console.log('Commands:\n')
+    matchedCommands.forEach(function (command) {
+        console.log('\t%s %s', command.command, command.options.map(function (option) { return '--' + option.name + ' ' + '<' + option.settings.describe + '>'}).join(' '));
+        console.log('\n%s\n', command.description);
+    });
+}
+
+var matchedCommands = parse(argv);
+
+/**
+ * returns command if command can be executed or false
+ *
+ * @param matchedCommands
+ * @param argv
+ * @returns {command|boolean}
+ */
+validate = function(matchedCommands, argv) {
+    if(matchedCommands.length != 1) return false;
+
+    var command = matchedCommands[0];
+
+    if (command.command != argv._.join(' ')) return false;
+
+    for (var i in command.options) {
+        var option = command.options[i];
+        argv.verbose && console.log('validate argument %s', option.name);
+        if(!argv.hasOwnProperty(option.name)) {
+            console.log('argument missing:\n\t<%s>', option.name);
+            return false;
+        }
+    }
+
+    return command;
+}
+
+if (argv.help) {
+    help(matchedCommands, argv);
+    process.exit(0);
+}
+
+var myCommand = validate(matchedCommands, argv);
+
+if (!myCommand) {
+    help(matchedCommands, argv);
+    process.exit(0);
+}
+
+executer = require('./commands/executor');
+Mustache = require('mustache');
 
 stdin = process.stdin;
 stdin.setEncoding('utf8');
@@ -25,3 +88,12 @@ stdin.on('data', function (data) {
     child.stdin.write(data);
 });
 stdin.resume();
+
+
+var batch = myCommand.mappedCommand.map(function (command) {
+    command.arguments = Mustache.render(command.arguments,argv);
+    return command;
+})
+
+//executing underlying command
+executer.executeBatch(batch, argv);
